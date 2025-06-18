9118c0e83eed1e60d6184c3944fd319fb3d41d2c
Ron Ferentini <ron.ferentini@gmail.com>
2025-06-18 13:15:42 -0400

    updated id for buttons and javascript to add item to cart when button is clicked.

--- js/script.js
+++ js/script.js
@@ -1,38 +1,36 @@
-const items = [
-  [1, "OBI RON'S Wheat", 39.99],
-  [2, "Sunny side Grapefuit Radler", 39.99],
-  [3, "Kolsch", 29.99],
-  [4, "American Blonde Ale", 32.0],
-  [5, "Amber Ale", 40.0],
-  [6, "Rasberry Sour", 64.99],
-  [7, "Mango Ale Sour", 64.99],
-  [8, "Vienna Lager", 35.0],
+const inventoryItems = [
+  ["1", "OBI RON'S Wheat", 39.99],
+  ["2", "Sunny side Grapefuit Radler", 39.99],
+  ["3", "Kolsch", 29.99],
+  ["4", "American Blonde Ale", 32.0],
+  ["5", "Amber Ale", 40.0],
+  ["6", "Rasberry Sour", 64.99],
+  ["7", "Mango Ale Sour", 64.99],
+  ["8", "Vienna Lager", 35.0],
 ];
 
 const cartContent = [];
-const sendButton1 = document.getElementById("button1");
-const sendButton2 = document.getElementById("button2");
-const sendButton3 = document.getElementById("button3");
-const sendButton4 = document.getElementById("button4");
-const sendButton5 = document.getElementById("button5");
-const sendButton6 = document.getElementById("button6");
-const sendButton7 = document.getElementById("button7");
-const sendButton8 = document.getElementById("button8");
+document.getElementById('item1').addEventListener('click', handleAddItem);
+document.getElementById('item2').addEventListener('click', handleAddItem);
+document.getElementById('item3').addEventListener('click', handleAddItem);
+document.getElementById('item4').addEventListener('click', handleAddItem);
+// document.getElementById('item5').addEventListener('click', handleAddItem);
+// document.getElementById('item6').addEventListener('click', handleAddItem);
+// document.getElementById('item7').addEventListener('click', handleAddItem);
+// document.getElementById('item8').addEventListener('click', handleAddItem);
 
-function handleSend() {
-  const itemid = sendButton1.value;
-  console.log("itemid: " + itemid);
-  for (let i = 0; i < items.length; i++) {
-    for (let j = 0; j < items[i].length; j++) {
-      if (itemid === items[i][j]) {
-        addCartItem(itemid);
-      }
+
+
+function handleAddItem(event) {
+  const itemId = event.target.value;
+
+  for (let i = 0; i < inventoryItems.length; i++) {
+    if (itemId === inventoryItems[i][0]) {
+        addCartItem(itemId);
+        break;
     }
   }
 }
-
-
-sendButton1.addEventListener("click", handleSend);
 
 // Validate that the email address entered on the contact form is validate form
 function validateEmail(email) {
@@ -41,12 +39,8 @@ function validateEmail(email) {
 
 // When the user clicks a Add to cart button add the item to the cart array
 function addCartItem(itemid) {
-  let newRow = [
-    { id: itemid, desc: items[itemid][1], price: items[itemid][2] },
-  ];
-  console.log("item: " + itemid);
-  cartContent.push(newRow);
-  console.log("cartContent " + cartContent);
+  let newItemToCart = [itemid, inventoryItems[itemid][1], inventoryItems[itemid][2]];
+  cartContent.push(newItemToCart);
 }
 
 // This funcion will populate the table on the cart html page with what has been addecd to the cart from the main html page
